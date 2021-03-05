d3b952d1b4e1a366e7b33ba8fcb783670fd998ec
Stefano <tepo@hotmail.it>
2021-03-05 10:11:26 +0100

    Fix

--- snack2-5/js/scripts.js
+++ snack2-5/js/scripts.js
@@ -5,8 +5,20 @@
 var listaLunga = [345, 7254, 7283, 873, 9, 64];
 var listaBreve = [23, 85];
 
-while (listaBreve.length < listaLunga.length) {
-  listaBreve.push(Math.floor(Math.random() * 10000));
+// while (listaBreve.length < listaLunga.length) {
+//   listaBreve.push(Math.floor(Math.random() * 10000));
+// }
+//
+// console.log(listaBreve);
+// console.log(listaLunga);
+
+
+while (listaBreve.length != listaLunga.length) {
+  if (listaBreve.length < listaLunga.length) {
+    listaBreve.push(Math.floor(Math.random() * 10000));
+  } else if (listaBreve.length > listaLunga.length) {
+    listaLunga.push(Math.floor(Math.random() * 10000));
+  }
 }
 
 console.log(listaBreve);
